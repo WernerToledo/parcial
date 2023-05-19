fcaca581300faa4d285aa35ad3cec9908a8de638
Alejandro Toledo <88223531+WernerToledo@users.noreply.github.com>
2023-05-19 10:06:23 -0600

    Trabajando en el bakc

---
+++ parcial/Controllers/OfertaController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using parcial.Models;
+
+namespace parcial.Controllers
+{
+    public class OfertaController : Controller
+    {
+        private readonly DBcontexto _ofertaContext;
+        public OfertaController(DBcontexto ofertaContext)
+        {
+            _ofertaContext = ofertaContext;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Buscar(string pNombre)
+        {
+            if(pNombre.IsNullOrEmpty())
+            {
+                return RedirectToAction("NotFound");
+            }
+            else
+            {
+                return View();
+            }
+
+            //var listadoTrabajos = (from o in _ofertaContext.oferta
+            //                       where o.tipo_trabajo.Contains(pNombre)
+            //                       select o).ToList();
+            //if (listadoTrabajos.Any())
+            //{
+            //    return View();
+            //}
+            //return RedirectToAction("NotFound");
+        }
+
+        public IActionResult NotFound() 
+        {
+            return View();
+        }
+    }
+}
